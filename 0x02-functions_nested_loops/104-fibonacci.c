07bd93cda9ce2b29fb2895572df002300351ceb7
tobialism <tobialism@gmail.com>
2023-08-16 04:42:58 -0700

    103-fibonacci.c added

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,17 +1,34 @@
-#include <stdio.h>
-int main(void)
-{
-  int i = 0;
-  unsigned long long fib1 = 0;
-  unsigned long long fib2 = 1;
-  unsigned long long fib_sum;
-  printf("1, 2, ");
-  for (i = 2; i < 98; i++) {
-    fib_sum = fib1 + fib2;
-    printf("%llu, ", fib_sum);
-    fib1 = fib2;
-    fib2 = fib_sum;
-  }
-  printf("\n");
-return (0);
-}
+#!/bin/bash
+# Description: Compile and run 104-fibonacci.c from header file
+
+# Constants
+CC="gcc"
+CFLAGS="-Wall -Werror -Wextra -pedantic"
+OUTPUT_EXEC="fib"
+HEADER_FILE="104-header.h"
+SOURCE_FILE="104-fibonacci.c"
+
+# Copy header into source file
+echo "#include<stdio.h>" > $SOURCE_FILE
+cat $HEADER_FILE >> $SOURCE_FILE
+
+# Compile
+$CC $CFLAGS $SOURCE_FILE -o $OUTPUT_EXEC
+
+if [ $? -ne 0 ]; then
+    echo "Error compiling $SOURCE_FILE" 
+    exit 1
+fi
+
+# Run
+./$OUTPUT_EXEC 
+
+if [ $? -ne 0 ]; then
+    echo "Error executing ./$OUTPUT_EXEC"
+    exit 2
+fi
+
+# Clean up
+rm $SOURCE_FILE $OUTPUT_EXEC
+
+exit 0
